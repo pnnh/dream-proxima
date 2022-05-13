1c108be0c81ca26aed45afd762c1c8dbea7a4ce3
larry <xspanni@gmail.com>
2022-05-13 11:12:33 +0800

    实现从数据库查询文章列表并渲染页面，注入对数据库和模板引擎的依赖

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,23 +1,70 @@
+use std::env;
+use std::sync::Arc;
+
 use axum::response::Html;
-use axum::{extract::Extension, http::StatusCode};
+use axum::{
+    extract::{Extension, Path},
+    http::{header, HeaderValue, StatusCode},
+    response::IntoResponse,
+    routing::get,
+    BoxError, Router,
+};
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
 use handlebars::Handlebars;
 use serde_json::json;
+use tokio_postgres::NoTls;
+use tower::ServiceBuilder;
+use tower_http::ServiceBuilderExt;
 
 use crate::layers;
 use crate::models;
 use crate::models::ArticleView;
 
-pub async fn using_connection_pool_extractor(
-    Extension(pool): Extension<layers::ConnectionPool>,
+#[derive(Clone, Debug)]
+struct State<'reg> {
+    registry: Arc<Handlebars<'reg>>,
+    pool: Arc<layers::ConnectionPool>,
+}
+
+pub async fn app() -> Router {
+    let dsn_env = env::var("DSN").expect("dsn_env is error");
+
+    let manager = PostgresConnectionManager::new_from_stringlike(dsn_env, NoTls).unwrap();
+    let pool = Pool::builder().build(manager).await.unwrap();
+
+    let mut reg = Handlebars::new();
+    reg.register_template_file("index", "assets/templates/index.hbs")
+        .unwrap();
+    reg.register_template_file("styles", "assets/templates/styles.hbs")
+        .unwrap();
+
+    let state = State {
+        registry: Arc::new(reg),
+        pool: Arc::new(pool),
+    };
+
+    let middleware = ServiceBuilder::new()
+        // Share the state with each handler via a request extension
+        .add_extension(state);
+
+    // Build route service
+    Router::new()
+        .route("/get", get(get_key))
+        .layer(middleware.into_inner())
+}
+
+async fn get_key<'a>(
+    Extension(state): Extension<State<'_>>,
 ) -> Result<Html<String>, (StatusCode, String)> {
-    let conn = pool.get().await.map_err(layers::internal_error)?;
+    let conn = state.pool.get().await.map_err(layers::internal_error)?;
 
     let query_result = conn
         .query("SELECT pk, title FROM articles limit 10", &[])
         .await
-        .map_err(layers::internal_error)?;
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
 
-    let mut models: Vec<models::ArticleView> = Vec::new();
+    let mut models: Vec<ArticleView> = Vec::new();
 
     for row in query_result {
         let pk: &str = row.get(0);
@@ -31,14 +78,10 @@ pub async fn using_connection_pool_extractor(
         models.push(model);
     }
 
-    let mut reg = Handlebars::new();
-    reg.register_template_file("index", "assets/templates/index.hbs")
-        .unwrap();
-    reg.register_template_file("styles", "assets/templates/styles.hbs")
-        .unwrap();
+    let result = state
+        .registry
+        .render("index", &json!({ "models": models }))
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
 
-    Ok(Html(
-        reg.render("index", &json!({ "models": models }))
-            .map_err(layers::internal_error)?,
-    ))
+    Ok(Html(result))
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,30 +1,9 @@
-use std::env;
 use std::net::SocketAddr;
-
-use axum::response::Html;
-use axum::{extract::Extension, routing::get};
-use bb8::Pool;
-use bb8_postgres::PostgresConnectionManager;
-use handlebars::Handlebars;
-use serde_json::json;
-use tokio_postgres::NoTls;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod handlers;
 mod layers;
 mod models;
-
-async fn index() -> Result<Html<String>, String> {
-    let mut reg = Handlebars::new();
-    reg.register_template_file("index", "assets/templates/index.hbs")
-        .unwrap();
-    let result = reg
-        .render("index", &json!({"name": ["World", "啊啊啊"]}))
-        .map_err(|err| err.to_string())?;
-    println!("{}", result);
-
-    Ok(Html(result))
-}
 
 #[tokio::main]
 async fn main() {
@@ -35,30 +14,11 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let dsn_env = env::var("DSN");
-    if dsn_env.is_err() {
-        tracing::error!("dsn_env is error {}", dsn_env.err().unwrap());
-        return;
-    }
-    let dsn = dsn_env.unwrap();
-
-    let manager = PostgresConnectionManager::new_from_stringlike(dsn, NoTls).unwrap();
-    let pool = Pool::builder().build(manager).await.unwrap();
-
-    let app = axum::Router::new()
-        .route("/", get(index))
-        .route("/hello", axum::routing::get(|| async { "Hello, World!" }))
-        .route(
-            "/tokio_postgres",
-            get(handlers::using_connection_pool_extractor),
-        )
-        .layer(Extension(pool));
-
     let addr = SocketAddr::from(([127, 0, 0, 1], 5500));
     tracing::debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(handlers::app().await.into_make_service())
         .await
         .unwrap();
 }
